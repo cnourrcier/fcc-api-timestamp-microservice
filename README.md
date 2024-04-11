847df555f7b6b258425392328a0b57beffd67395
Charlie Nourrcier <cnourrcier@hotmail.com>
2024-04-11 16:38:51 -0500

    Project submission

--- README.md
+++ README.md
@@ -1,3 +1,44 @@
 # Timestamp Microservice
 
-This is the boilerplate code for the Timestamp Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice
+This is my Timestamp Microservice Back End Development and APIs Project for freecodecamp.org. I used the provided [boilerplate](https://github.com/freeCodeCamp/boilerplate-project-timestamp/) to complete this challenge. The test requirements can be found [here](https://www.freecodecamp.org/learn/back-end-development-and-apis/back-end-development-and-apis-projects/timestamp-microservice).
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [API Endpoints](#api-endpoints)
+- [License](#license)
+
+## Installation
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/cnourrcier/fcc-api-timestamp-microservice.git
+```
+
+2. Install dependencies:
+
+```bash
+cd fcc-api-timestamp-microservice
+npm install
+```
+
+## Usage
+
+1. Start the server:
+
+```bash
+npm run dev
+```
+
+## API Endpoints
+
+|   Endpoint    |  Description  |
+| ------------- | ------------- |
+| GET /api/{dateParam} | Receive a valid date param and send a json object containing a unix timestamp and UTC date string. If date param is not valid, send an error response. |
+
+## License
+
+This project is licensed under the MIT License - see the [MIT License](LICENSE) file for details. 
+
--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+const cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,15 +18,26 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function (req, res) {
+  let { date } = req.params;
+  if (!date) {
+    date = new Date(); // if no date param, datetime is current datetime.
+  }
+  if (/^\d+$/.test(date)) { // Check if date is only digits 0-9
+    const unixTime = parseInt(date); // Convert to number type
+    const utcTime = new Date(unixTime).toUTCString(); // Convert date to the RFC 7231 format UTC
+    res.json({ unix: unixTime, utc: utcTime });
+  } else if (new Date(date) != 'Invalid Date') { // If date format is valid
+    const unixTime = Date.parse(date); // Convert date to unix (number of milliseconds since the epoch)
+    const utcTime = new Date(date).toUTCString(); // Convert date to the RFC 7231 format UTC
+    res.json({ unix: unixTime, utc: utcTime });
+  } else {
+    res.json({ error: "Invalid Date" })
+  }
 });
 
-
-
 // Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
